125fc93cea34a9e9a252759055417ead48f6d9b3
Trung_Dong <letrungdong1997.bg@gmail.com>
2019-07-04 21:08:28 +0700

    relative layout and object

--- HelloWord/app/src/main/java/com/example/helloword/MainActivity.java
+++ HelloWord/app/src/main/java/com/example/helloword/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        //taodoi tuong tu khuon Student
+        //b1 khai bao
+        Student sv1;
+        sv1=new Student();
+        sv1.name = "Le Trung Dong";
+        sv1.age=22;
+        sv1.schoolName="Unknown";
+        sv1.id=122272987;
+
+        Toast.makeText( this, sv1.name, Toast.LENGTH_SHORT ).show();
+
     }
 }
---
+++ HelloWord/app/src/main/java/com/example/helloword/Student.java
@@ -0,0 +1,13 @@
+package com.example.helloword;
+
+public class Student {
+    String name;
+    int age;
+    String  schoolName;
+    int id;
+
+
+
+
+
+}
